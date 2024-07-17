dab91761a013bfd74ab8726acbc90491b6e124c7
JoWatersASC <joshua.waters365@gmail.com>
2024-07-17 12:00:11 -0400

    First Commit

---
+++ src/sorting_algos.cpp
@@ -0,0 +1,22 @@
+#include"sorting_algos.h"
+
+namespace mySort
+{
+template<typename T>
+void insertionSort(std::vector<T> vect){}
+
+template<typename T>
+void selectionSort(std::vector<T> vect){}
+
+template<typename T>
+void bubbleSort(std::vector<T> vect){}
+
+template<typename T>
+void quickSort(std::vector<T> vect){}
+
+template<typename T>
+void mergeSort(std::vector<T> vect){}
+
+template<typename T>
+void heapSort(std::vector<T> vect){}
+}
